0bf45dc9846db7251c023563d1a119882cdf068e
Lynne Bones <lbones@il-d3.msk.21-school.ru>
2021-02-19 20:39:09 +0300

    src v2

---
+++ srcs/autoindex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if grep -q "autoindex on" /etc/nginx/sites-available/default
+then
+	sed -i "s/autoindex on/autoindex off/g" /etc/nginx/sites-available/default
+	service nginx restar
+elif grep -q "autoindex off" /etc/nginx/sites-available/default
+then
+	sed -i "s/autoindex off/autoindex on/g" /etc/nginx/sites-available/default
+	service nginx restar
+fi
---
+++ srcs/init.sh
@@ -0,0 +1,11 @@
+service nginx start
+service mysql start
+service php7.3-fpm start
+
+# Configure a wordpress database
+echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
+echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
+
+bash
