7a3b7190286073e9543b16267b4642e520af5995
Lynne Bones <lbones@il-a1.msk.21-school.ru>
2021-02-20 19:01:09 +0300

    autoindex.sh fixed

--- srcs/autoindex.sh
+++ srcs/autoindex.sh
@@ -2,9 +2,9 @@
 if grep -q "autoindex on" /etc/nginx/sites-available/default
 then
 	sed -i "s/autoindex on/autoindex off/g" /etc/nginx/sites-available/default
-	service nginx restar
+	service nginx restart
 elif grep -q "autoindex off" /etc/nginx/sites-available/default
 then
 	sed -i "s/autoindex off/autoindex on/g" /etc/nginx/sites-available/default
-	service nginx restar
+	service nginx restart
 fi
