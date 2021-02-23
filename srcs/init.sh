b078500caaa7153959f0654a2d9478cf2067a49d
Lynne Bones <lbones@il-j3.msk.21-school.ru>
2021-02-23 23:14:01 +0300

    init.sh

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,4 +1,3 @@
-service nginx start
 service mysql start
 service php7.3-fpm start
 
@@ -7,5 +6,4 @@ echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
 echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
-
-bash
+nginx -g "daemon off;"
